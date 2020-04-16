b72bfaae491638c6fea18536ef523ed04c0250b1
Alex Ozdemir <aozdemir@hmc.edu>
2020-04-15 19:52:22 -0700

    file_value test

---
+++ tools/pygg/examples/fib_files.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3.7
+# ARGS: fib 5
+# RESULT: 5
+# Copied
+import os, sys
+from importlib.util import spec_from_loader, module_from_spec
+from importlib.machinery import SourceFileLoader
+
+if not os.path.exists(sys.argv[1]):
+    import pygg
+else:
+    spec = spec_from_loader(
+        "pygg", SourceFileLoader("pygg", os.path.realpath(sys.argv[1]))
+    )
+    pygg = module_from_spec(spec)
+    spec.loader.exec_module(pygg) # type: ignore
+    del sys.argv[1]
+
+# End copy
+
+
+@pygg.thunk_fn()
+def fib(gg: pygg.GG, n: int) -> pygg.Output:
+    if n < 2:
+        return gg.str_value(str(n))
+    else:
+        a = gg.thunk(fib, [n - 1])
+        b = gg.thunk(fib, [n - 2])
+        return gg.thunk(add_str, [a, b])
+
+
+@pygg.thunk_fn()
+def add_str(gg: pygg.GG, a: pygg.Value, b: pygg.Value) -> pygg.Output:
+    ai = int(a.as_str())
+    bi = int(b.as_str())
+    path = 'random'
+    with open(path, 'w') as f:
+        f.write(str(ai + bi))
+    return gg.file_value(path)
+
+
+pygg.main()
