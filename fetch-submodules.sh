88bba0a54e2520b35a65e8bff847d38b3a7bf298
Alex Ozdemir <aozdemir@hmc.edu>
2021-01-08 23:51:05 -0800

    get-deps

--- fetch-submodules.sh
+++ fetch-submodules.sh
@@ -3,8 +3,3 @@
 git submodule update --recursive --init test_vectors
 git submodule update --init toolchain
 git submodule update --init third_party/cpptoml
-./contrib/build-openssl.sh
-./contrib/build-cryptopp.sh
-git submodule update --init third_party/hiredis
-./contrib/patch-hiredis.sh
-./contrib/get-protobuf.sh
